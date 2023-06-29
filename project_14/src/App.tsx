f31067b83763481f1d6b1dd90356a681bebbcbb6
sanjay v <vsanjay8722@gmail.com>
2023-06-29 18:30:16 +0530

    done

--- project_14/src/App.tsx
+++ project_14/src/App.tsx
@@ -3,11 +3,20 @@ import { useGlobalContext } from './context'
 
 
 import Navbar from './Navbar'
-import CartContainer from './CartContainer'
+import CartContainer from './CartContainer';
+
+
 
 
 function App() {
-
+const {loading}=useGlobalContext();
+if(loading){
+  return (
+    <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  )
+}
   return (
     <main>
       <Navbar />
--- project_14/src/context.tsx
+++ project_14/src/context.tsx
@@ -1,33 +1,97 @@
-import React, { ReactNode, useState ,useContext } from "react";
-import { data, DataType } from "./data";
-import App from "./App";
+import React, { useContext, useEffect, useReducer } from "react";
+import reducer from "./reducer";
+import { DataType } from "./data";
 
-const url: string = "https://course-api.com/react-useReducer-cart-project";
 
-type AppContextType={
-    cart:DataType[]
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+interface ContextType {
+  loading: boolean;
+  cart:DataType [];
+  total: number;
+  amount: number;
+  clearCart: () => void;
+  remove: (id: any) => void;
+  increase: (id: any) => void;
+  decrease: (id: any) => void;
+  fetchData: () => void;
+  toggleAmount: (id: any, type: any) => void;
 }
 
-const AppContext = React.createContext<AppContextType>({
-    cart:[]
+const AppContext = React.createContext<ContextType>({
+  loading: false,
+  cart: [],
+  total: 0,
+  amount: 0,
+  clearCart: () => {},
+  remove: (id: any) => {},
+  increase: (id: any) => {},
+  decrease: (id: any) => {},
+  fetchData: () => {},
+  toggleAmount: (id: any, type: any) => {},
 });
 
-const AppProvider = ({ children }: { children: ReactNode }) => {
-  const [cart, setCart] = useState(data);
+const initialState = {
+  loading: false,
+  cart: [],
+  total: 0,
+  amount: 0,
+};
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+  const remove = (id : any) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+  const increase = (id : any) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+  const decrease = (id : any) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+  const toggleAmount = (id : any, type : any) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
   return (
     <AppContext.Provider
       value={{
-        cart
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        toggleAmount,
       }}
     >
       {children}
     </AppContext.Provider>
   );
 };
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
 
-export const useGlobalContext=()=>{
-    return useContext(AppContext);
-}
+export { AppContext, AppProvider };
 
-
-export default AppProvider;
